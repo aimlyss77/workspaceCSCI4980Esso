46444004214b11c10021eea49b8279f534dace27
Aimlys Esso <aesso@unomaha.edu>
2018-09-04 17:16:59 -0500

    Commit

---
+++ project0904-table-view-example-esso/src/viewer/MyTableViewer.java
@@ -0,0 +1,14 @@
+
+package viewer;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class MyTableViewer {
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+
+}
